5e29eb901f59f833b4a9850a8cd28ea03eb65357
Devil@69 <n8nconsistent@gmail.com>
2026-02-14 18:02:06 +0530

    party

--- party/models.py
+++ party/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     country = models.CharField(max_length=100)
     customer_type = models.CharField(max_length=50)
     interested_items = models.JSONField(default=list)
--- party/urls.py
+++ party/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_customer, name='add_customer'),
     path('edit/<int:id>/', views.edit_customer, name='edit_customer'),
-    path('delete/<int:id>/', views.delete_customer, name='delete_customer'),
+    path('delete/<int:id>/', views.hard_delete_customer, name='hard_delete_customer'),
+    path('disable/<int:id>/', views.disable_customer, name='disable_customer'),
+
 ]
